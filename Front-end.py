73a9c1ed21205650d3b575d2cc5ca114092a82c6
30client <30client@lyceum130.ru>
2023-11-28 09:23:44 +0500

    added output in exe

--- Front-end.py
+++ Front-end.py
@@ -26,69 +26,86 @@ class YouTubeStatisticsApp(QMainWindow):
 
     
     def get_statistics(self):
+        
+        vbox = QVBoxLayout()
+        widget = QWidget()
         channel_id = self.input_field.text()
         
-        # Создаем YouTube Data API клиент
-        api_key = 'AIzaSyBHouVubyJV00ehrCoGSFGoyIxGZjl4pFA'
-        youtube = build('youtube', 'v3', developerKey=api_key)
+        try:
+            # Создаем YouTube Data API клиент
+            api_key = 'AIzaSyBHouVubyJV00ehrCoGSFGoyIxGZjl4pFA'
+            youtube = build('youtube', 'v3', developerKey=api_key)
 
-        # Получаем статистику канала
-        response = youtube.channels().list(
-            part='statistics',
-            id=channel_id
-        ).execute()
-        # Получаем количество подписок и сумму просмотров
-        sub_count = response['items'][0]['statistics']['subscriberCount']
-        view_count = response['items'][0]['statistics']['viewCount']
-        
-        # Получаем среднее количество просмотров
-        video_response = youtube.videos().list(
-            part='statistics',
-            chart='mostPopular',
-            maxResults=10
-        ).execute()
+            # Получаем статистику канала
+            response = youtube.channels().list(
+                part='statistics',
+                id=channel_id
+            ).execute()
+            # Получаем количество подписок и сумму просмотров
+            sub_count = response['items'][0]['statistics']['subscriberCount']
+            view_count = response['items'][0]['statistics']['viewCount']
+            
+            # Получаем среднее количество просмотров
+            video_response = youtube.videos().list(
+                part='statistics',
+                chart='mostPopular',
+                maxResults=10
+            ).execute()
 
-        total_views = 0
-        total_likes = 0
-        most_viewed_videos = []
-        
-        # Получаем список видео канала
-        video_response = youtube.search().list(
-            part='id',
-            channelId=channel_id,
-            maxResults=10
-        ).execute()
+            total_views = 0
+            total_likes = 0
+            most_viewed_videos = []
+            
+            # Получаем список видео канала
+            video_response = youtube.search().list(
+                part='id',
+                channelId=channel_id,
+                maxResults=10
+            ).execute()
 
-        # Получаем статистику для каждого видео
-        for item in video_response.get('items', []):
-            if 'videoId' in item['id']:
-                video_id = item['id']['videoId']
-                video_info = youtube.videos().list(
-                    part='statistics, snippet',
-                    id=video_id
-                ).execute()
-                if video_info.get('items'):
-                    title = video_info['items'][0]['snippet']['title']
-                    views = video_info['items'][0]['statistics']['viewCount']
-                    likes = video_info['items'][0]['statistics']['likeCount']
-                    most_viewed_videos.append(title)
-                    total_views += int(views)
-                    total_likes += int(likes)
+            # Получаем статистику для каждого видео
+            for item in video_response.get('items', []):
+                if 'videoId' in item['id']:
+                    video_id = item['id']['videoId']
+                    video_info = youtube.videos().list(
+                        part='statistics, snippet',
+                        id=video_id
+                    ).execute()
+                    if video_info.get('items'):
+                        title = video_info['items'][0]['snippet']['title']
+                        views = video_info['items'][0]['statistics']['viewCount']
+                        likes = video_info['items'][0]['statistics']['likeCount']
+                        most_viewed_videos.append(title)
+                        total_views += int(views)
+                        total_likes += int(likes)
 
-        average_views = total_views / len(video_response.get('items', []))
-        
-        # Выводим результаты
-        print(f'Количество подписок: {sub_count}')
-        print(f'Среднее количество просмотров: {average_views}')
-        print(f'Сумма просмотров: {total_views}')
-        print(f'Сумма лайков: {total_likes}')
-        print('Самые просматриваемые видео:')
-        for video in most_viewed_videos:
-            print(video)
+            average_views = total_views / len(video_response.get('items', []))
+            
+            
+            
+            # Выводим результаты
+            vbox.addWidget(QLabel(f'Количество подписок: {sub_count}'))
+            vbox.addWidget(QLabel(f'Среднее количество просмотров: {average_views}'))
+            vbox.addWidget(QLabel(f'Сумма просмотров: {total_views}'))
+            vbox.addWidget(QLabel(f'Сумма лайков: {total_likes}'))
+            vbox.addWidget(QLabel('Самые просматриваемые видео:'))
+            for video in most_viewed_videos:
+                vbox.addWidget(QLabel(video))
+            widget.setLayout(vbox)
+            self.setCentralWidget(widget)
+            return 0
+        except:
+            vbox.addWidget(QLabel(f'Неверный ID канала youtube'))
+            vbox.addWidget(self.label)
+            self.input_field = QLineEdit()
+            self.button = QPushButton('Получить статистику')
+            widget.setLayout(vbox)
+            self.setCentralWidget(widget)
+            return 1
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = YouTubeStatisticsApp()
-    ex.resize(250, 150)
+    ex.resize(350, 250)
     ex.show()
     sys.exit(app.exec_())
